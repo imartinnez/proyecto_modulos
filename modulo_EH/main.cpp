4b9e0eb4687a435421ebab625f474ed447e1192a
imartinnez <martinezinigo34@gmail.com>
2024-05-25 13:00:59 +0200

    commit 7

---
+++ modulo_EH/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "refrescarTXT.hpp"
+#include "json.hpp"
+using namespace std;
+int main(){
+    //Database db = Database();
+    //RefrescarTXT prueba(db);
+    //prueba.filterCityWeather();
+    return 0;
+}
